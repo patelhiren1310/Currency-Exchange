f82ffa6ef33ac71ac5ab26d2ebb850321787fbbe
patelhiren1310 <121115953+patelhiren1310@users.noreply.github.com>
2024-02-04 08:45:54 +0530

    Add files via upload
    Currency Exchange practice done.
    During MySQL error handled.

---
+++ data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange(id,conversion_multiple,environment,currency_from,currency_to)
+values (10001,"65","","USD","INR"),
+(10002,"75","","ASD","INR"),
+(10003,"35","","URO","INR");
